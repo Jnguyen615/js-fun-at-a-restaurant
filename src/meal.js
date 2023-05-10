4730738182eed7fb9838c4742f36fcac1cc52f72
Jen Nguyen <jenniferannnguyen@live.com>
2023-05-10 16:00:54 -0600

    Initial Commit

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,29 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItem) {
+    return `Delicious ${menuItem}`
 }
+
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(ingredient, ingredients) {
+    ingredients.push(ingredient)
+   if (ingredients.includes(ingredient)) {
+    
+   }
+}
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe

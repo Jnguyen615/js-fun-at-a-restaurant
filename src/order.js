c0cc999853f05b4e0606c4ec4ee95167817edf9a
Jen Nguyen <jenniferannnguyen@live.com>
2023-05-10 20:04:20 -0600

    Add listItems Function

--- src/order.js
+++ src/order.js
@@ -4,12 +4,33 @@ function takeOrder (order, deliveryOrders) {
   }
 }
 
-function refundOrder() {
-
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++){
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1)
+    }
+  }
 }
+
+function listItems(deliveryOrders) {
+  var items = ''
+  var lastIndex =  deliveryOrders.length -1
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i] === deliveryOrders[lastIndex]) {
+      items += deliveryOrders[i].item 
+    } else {
+      items += deliveryOrders[i].item 
+      items += ', '
+    }
+  }
+    return items
+}
+      
+
+    
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
+  listItems,
   // searchOrder
 }

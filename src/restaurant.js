1d23f795f296d8e5e2ec1d19d49c05e274c3f546
Jen Nguyen <jenniferannnguyen@live.com>
2023-05-10 18:19:12 -0600

    Add createRestaurant and start addMenuItem functions

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,28 @@
+function createRestaurant(name) {
+  return {
+    name: name,
+      menus: {
+        breakfast: [],
+        lunch: [],
+        dinner: []
+    }
+  }
+}
 
+function addMenuItem(restaurant, item) {
+  if (item.type === 'lunch' && !restaurant.menus.lunch.includes(item)) {  
+      restaurant.menus.lunch.push(item)
+    } else if 
+      (item.type === 'breakfast' && !restaurant.menus.breakfast.includes(item)) {
+      restaurant.menus.breakfast.push(item) 
+    } else {
+      restaurant.menus.dinner.push(item)
+    }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem,
   // checkForFood
 }
